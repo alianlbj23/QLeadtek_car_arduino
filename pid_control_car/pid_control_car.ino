8854d4bc1e8128a60263199b620f614a2ed06aad
alianlbj23 <alianlbj23@gmail.com>
2024-10-16 00:58:21 +0800

    refactor

--- pid_control_car/pid_control_car.ino
+++ pid_control_car/pid_control_car.ino
@@ -1,30 +1,26 @@
 #include <ArduinoJson.h>
 #include <Servo.h>
 #include "MotorDriver.h"
-#define DEVICE_TYPE "MOTOR_CONTROLLER"
-#define CUSTOM_ID "MOTOR001"
+#define CUSTOM_ID "usb_rear_wheel"
 #define MOTORTYPE YF_IIC_RZ  // rz7889
-uint8_t SerialDebug = 1;
-
 const int offsetm1 = 1;
 const int offsetm2 = 1;
 const int offsetm3 = 1;
 const int offsetm4 = 1;
-
-const int encoderPinA = 3; // 假设编码器连接到引脚3
+const int encoderPinA = 3;
 volatile int encoderTicks = 0;
 unsigned long lastTime = 0;
-const int encoderTicksPerRevolution = 20; // 每圈的编码器脉冲数
-
+const int encoderTicksPerRevolution = 20;
 MotorDriver motorDriver = MotorDriver(MOTORTYPE);
 Servo servo = Servo();
 
 void setup() {
   Serial.begin(115200);
   Serial.println(CUSTOM_ID);
+  delay(1000);
   motorDriver.begin();
   motorDriver.motorConfig(offsetm1, offsetm2, offsetm3, offsetm4);
-  motorDriver.setPWMFreq(50); // 控制舵机时，需要设置PWM频率 ~50
+  motorDriver.setPWMFreq(50);
   servo.attach(10);
 
   pinMode(encoderPinA, INPUT);
@@ -37,13 +33,13 @@ void encoderISR() {
 }
 
 void controlLeftWheels(int speed) {
-  motorDriver.setSingleMotor(M3, -speed);  // 控制后轮
-  motorDriver.setSingleMotor(M4, -speed);  // 控制后轮
+  motorDriver.setSingleMotor(M3, -speed);  // rear
+  motorDriver.setSingleMotor(M4, -speed);  // rear
 }
 
 void controlRightWheels(int speed) {
-  motorDriver.setSingleMotor(M1, speed);  // 控制前轮
-  motorDriver.setSingleMotor(M2, speed);  // 控制前轮
+  motorDriver.setSingleMotor(M1, speed);  // front
+  motorDriver.setSingleMotor(M2, speed);  // front
 }
 
 int wheelSpeedRatio(int speed) {
@@ -68,16 +64,16 @@ void loop() {
       Serial.println(CUSTOM_ID);
     }
     else{
-      Serial.println("dsadsad");
       JsonArray targetVelArray = doc["target_vel"];
       if (targetVelArray.size() == 2) {
         int rearWheelSpeed = wheelSpeedRatio(targetVelArray[0]);
         int frontWheelSpeed = wheelSpeedRatio(targetVelArray[1]);
         controlLeftWheels(rearWheelSpeed);
         controlRightWheels(frontWheelSpeed);
-      } else {
-        Serial.println("Invalid target_vel array size");
-      }
+      } 
+      // else {
+      //   Serial.println("Invalid target_vel array size");
+      // }
     }
     
   }
